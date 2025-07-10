c61ad0878518a5347f2e274524648e020621391b
Luca Faccenda <lucafaccenda@Lucas-M3-MacBook-Pro.local>
2025-07-10 21:41:31 +0100

    adding a test folder and the start of the Card class definitions. A testing framework still needs to be chosen and implemented before moving further into developing functions for the class.

---
+++ src/Card/Card.cpp
@@ -0,0 +1,7 @@
+
+#include "Card.h"
+
+void Card::print_test()
+{
+    std::cout<<Attrabute.Movement_Speed<<std::endl;
+}
---
+++ src/include/definitions.h
@@ -0,0 +1,12 @@
+
+//Card Types
+#define Minor_Hero 0
+#define Major_Hero 1
+#define Exhaust_Action 2
+#define Free Action 3
+#define Combat_Action 4
+
+
+//Define Default Card values that are only excepted by specific cards
+#define Default_Speed 2;
+
