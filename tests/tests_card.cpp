3ffbd22cbca2b78757e09721cf14cffc4fa25c8c
Luca Faccenda <lucafaccenda@Lucas-M3-MacBook-Pro.local>
2025-07-12 16:23:59 +0100

    Resyncing tests

--- tests/tests_card.cpp
+++ tests/tests_card.cpp
@@ -2,19 +2,38 @@
 #include <gtest/gtest.h>
 #include "Card.h"
 
+
+//Attrabute inititalisations
 TEST(CardTest,Attribute_Movement_inititalisation){
-    Card c;
-    EXPECT_EQ(c.Attrabute.Movement_Speed,2);
+    Attributes a;
+    EXPECT_EQ(a.Movement_Speed,2);
 };
 
-
+//Condition inititalisations
 TEST(CardTest,Conditions_Unblockable_inititalisation){
-    Card c;
-    EXPECT_FALSE(c.Condition.Unblockable);
+    Conditions c;
+    EXPECT_FALSE(c.Unblockable);
 };
 
 TEST(CardTest,Conditions_ready_inititalisation){
-    Card c;
-    EXPECT_TRUE(c.Condition.Ready);
+    Conditions c;
+    EXPECT_TRUE(c.Ready);
 };
+
+//Setting Functions
+TEST(CardTest,Setting_Attrabutes){
+    Card C;
+    int attack = 4;
+    int defence = 2;
+    int movement_speed = 3;
+    C.fill_attributes(attack,defence,movement_speed);
+    EXPECT_EQ(C.Attrabute.Attack,attack);
+    EXPECT_EQ(C.Attrabute.Defence,defence);
+    EXPECT_EQ(C.Attrabute.Movement_Speed,movement_speed);
+    
+    C.fill_attributes(attack,defence);
+    EXPECT_EQ(C.Attrabute.Attack,attack);
+    EXPECT_EQ(C.Attrabute.Defence,defence);
+    EXPECT_EQ(C.Attrabute.Movement_Speed,Default_Speed);
+}
 
