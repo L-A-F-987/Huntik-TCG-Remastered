b2e92782f51da6aa5d0d64367b8dcf3f45c8b1ef
Luca Faccenda <lucafaccenda@Lucas-M3-MacBook-Pro.local>
2025-08-13 15:38:04 +0100

    updating Card class to include constructor which generates card type, updated tests accordingly

--- src/Board/Board.cpp
+++ src/Board/Board.cpp
@@ -1,6 +1,3 @@
 #include "Board.h"
 
 
-void Board::Play_Hero_Card(Card* Played_Card, bool Player_Id){
-
-};
--- src/include/Board.h
+++ src/include/Board.h
@@ -6,16 +6,8 @@
 class Board{
 
     public:
-        int x;
-
-        void Play_Hero_Card(Card* Played_Card, bool Player_Id);
 
     private:
-        std::vector<Card> Row_1;
-        std::vector<Card> Row_2;
-        std::vector<Card> Row_3;
-        std::vector<Card> Row_4;
-        std::vector<Card> Row_5;
 
 };
 
