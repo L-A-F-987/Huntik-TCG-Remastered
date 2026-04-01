75b2c7542e36d46fc1b71b26ffcdbad85a15e6de
L-A-F-987 <lucafaccenda22@gmail.com>
2026-04-01 22:28:14 +0100

    adding starting logic, still need to add unit tests

--- src/include/Deck.h
+++ src/include/Deck.h
@@ -3,12 +3,29 @@
 
 #include "Card.h"
 #include <random>
+#include <thread>
+#include <deque>
+#include <algorithm>
 
 class Deck{
 
     public:
+        // constructor
+        Deck(Card* cards_location, std::string provided_name = "none");
 
+        // getters
+        int GetDeckSize();
+
+        // public variables
+        std::deque<Card> deck;
+        std::string name;
     private:
+        // setters
+        void Deck::FillDeck(Card* starting_cards);
+
+        // private variables
+        std::thread thr_deck_management;
+        std::random_device seed;
 
 };
 
