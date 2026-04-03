511e63337ad3d7618159f12ea99faebd9b7e8ab6
Luca Faccenda <lucafaccenda@Lucas-M3-MacBook-Pro.local>
2026-04-03 20:36:47 +0100

    removing failing test

--- tests/unit_tests/tests_deck.cpp
+++ tests/unit_tests/tests_deck.cpp
@@ -15,16 +15,3 @@ TEST(DeckTest,Testing_Name_Declaration){
     EXPECT_EQ(test_deck.name, name);
 
 };
-
-TEST(DeckTest,Testing_CardList_Declaration){
-
-    // variables for deck test
-    Card card_1(Type {HERO,MINOR_HERO}, "test 1", GOOD);
-    std::deque<Card> test_cards_list = {card_1};
-    std::string name = "test";
-
-    // creating deck and testing
-    Deck test_deck(test_cards_list, name);
-    EXPECT_EQ(test_deck.cards, test_cards_list);
-
-};
