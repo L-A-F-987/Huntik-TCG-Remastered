5d9c7ca1f1a4856ff2fc0573df6d2d569d824844
Luca Faccenda <lucafaccenda@Lucas-M3-MacBook-Pro.local>
2026-05-17 13:23:52 +0100

    Refactor and new unit tests

--- src/include/Deck.h
+++ src/include/Deck.h
@@ -3,35 +3,40 @@
 
 #include "Card.h"
 #include <random>
-#include <thread>
 #include <deque>
 #include <algorithm>
 #include <optional>
+
+#define TOP_DECK 0
+#define BOTTOM_DECK -1
 
 class Deck{
 
     public:
         // constructor
-        Deck(std::deque<Card> starting_cards, std::string provided_name = "none");
+        Deck(std::deque<Card*> starting_cards,
+            std::string provided_name = "none",
+            bool shuffle = true
+        );
 
         // getters
         int GetDeckSize();
         std::string GetDeckName();
 
         // functions for modiying deck
-        std::optional<Card> DrawCard();
+        std::optional<Card*> DrawCard();
+        void AddToDeck(Card* card,
+            int location = BOTTOM_DECK
+        );
         void Shuffle();
 
-        // public variables
-        std::deque<Card> cards;
-        std::string name;
-
     private:
-        // setters
 
         // private variables
-        std::thread thr_deck_management;
         std::random_device seed;
+        std::mt19937 generator;
+        std::string name;
+        std::deque<Card*> cards;
 
 };
 
