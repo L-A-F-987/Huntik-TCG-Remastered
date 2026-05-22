9a132d2d91a7581f035c0064f3d4e912fa387d2d
Luca Faccenda <lucafaccenda@Lucas-M3-MacBook-Pro.local>
2026-05-22 17:49:23 +0100

    waiting for Card IDs to be implemented before moving further

--- tests/unit_tests/tests_hand.cpp
+++ tests/unit_tests/tests_hand.cpp
@@ -5,7 +5,6 @@ TEST(HandTest, Test_Add_Card_Succeeds){
     Card card_1(Type {HERO,MINOR_HERO}, "test 1", GOOD);
     Hand test_hand;
     EXPECT_NO_THROW(test_hand.AddCard(&card_1));
-
 }
 
 TEST(HandTest, Test_Add_Card_Increments_n_Cards_by_1){
@@ -15,5 +14,30 @@ TEST(HandTest, Test_Add_Card_Increments_n_Cards_by_1){
     test_hand.AddCard(&card_1);
 
     EXPECT_EQ(test_hand.GetHandSize(), initial_hand_size + 1);
+}
 
+TEST(HandTest, Test_Remove_Card_Succeeds){
+    Card card_1(Type {HERO,MINOR_HERO}, "test 1", GOOD);
+    Hand test_hand;
+    test_hand.AddCard(&card_1);
+    EXPECT_NO_THROW(test_hand.RemoveCard(&card_1));
+}
+
+TEST(HandTest, Test_Remove_Card_Fails_For_Empty_Hand){
+    Card card_1(Type {HERO,MINOR_HERO}, "test 1", GOOD);
+    Hand test_hand;
+    EXPECT_THROW(
+        test_hand.RemoveCard(&card_1),
+        std::runtime_error
+    );
+}
+
+TEST(HandTest, Test_Remove_Card_Increments_n_Cards_by_1){
+    Card card_1(Type {HERO,MINOR_HERO}, "test 1", GOOD);
+    Hand test_hand;
+    test_hand.AddCard(&card_1);
+    int initial_hand_size = test_hand.GetHandSize();
+    test_hand.RemoveCard(&card_1);
+
+    EXPECT_EQ(test_hand.GetHandSize(), initial_hand_size - 1);
 }
